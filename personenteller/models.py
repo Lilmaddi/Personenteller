62be67f5fa084c5e321b237dfcaf6cdc861e8218
dorienk97 <Dorienkuperus@gmail.com>
2021-04-22 21:09:00 +0200

    master

--- personenteller/models.py
+++ personenteller/models.py
@@ -2,30 +2,6 @@ from personenteller import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, login_required, logout_user
 
-class Type(db.Model):
-
-    __tablename__ = 'type'
-
-    id = db.Column(db.Integer,primary_key = True) #type bungalow
-    pers = db.Column(db.Integer)
-    prijs = db.Column(db.Integer)
-
-    def __init__(self,pers,prijs):
-        self.pers = aantalpersonen
-        self.prijs = weekprijs
-
-class Accomodatie(db.Model):
-
-    __tablename__ = 'accomodatie'
-
-    id = db.Column(db.Integer,primary_key = True) #type bungalow
-    naam = db.Column(db.Text)
-    type = db.Column(db.Integer,db.ForeignKey('type.id'))
-
-    def __init__(self,pers,prijs):
-        self.naam = naam
-        self.type = type
-
 @login_manager.user_loader
 def load_user(klant_id):
     return Klant.query.get(klant_id)
